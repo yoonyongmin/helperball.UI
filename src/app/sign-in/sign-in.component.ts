879e5ed43015c2362a7851fc1144d80ed9ca32b7
yoonyongmin <dydalszhffk@naver.com>
2021-10-19 16:56:24 +0900

    sign-in 컴포넌트 생성 및 saveUser API 호출 함수 구현

---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, FacebookLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { HelperballService } from '../service/helperball.service';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+
+  loginForm: FormGroup;
+  socialUser: SocialUser;
+  isLoggedin: boolean;
+
+  token;
+  name;
+  email;
+
+  constructor(
+    private helperballService: HelperballService, 
+    private router: Router,
+    private formBuilder: FormBuilder, 
+    private socialAuthService: SocialAuthService,
+  ) { }
+
+    ngOnInit() {
+      this.loginForm = this.formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      });    
+      
+      // this.socialAuthService.authState.subscribe((user) => {
+      //   this.socialUser = user;
+      //   this.isLoggedin = (user != null);
+      //   console.log(this.socialUser);
+      //   this.token = user.idToken;
+      //   this.name = user.name;
+      //   this.email = user.email;
+      // });
+    }
+    
+    loginWithGoogle(): void {
+      this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+      this.socialAuthService.authState.subscribe((user) => {
+        this.socialUser = user;
+        this.isLoggedin = (user != null);
+        console.log(this.socialUser);
+        this.token = user.id;
+        this.name = user.name;
+        this.email = user.email;
+
+        this.helperballService.saveUser(this.token, this.name, this.email).subscribe(res => {
+          console.log(res);
+          console.log('로그인 정보 저장');
+          this.router.navigate(['/dashboard']);
+        })
+      })
+    }
+    
+    loginWithFacebook(): void {
+      this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    }
+
+}
