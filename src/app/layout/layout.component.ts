81299b5128a5d1f99b766a0550481f93895ce844
yoonyongmin <dydalszhffk@naver.com>
2021-10-19 16:56:41 +0900

    layout 컴포넌트 생성

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { GoogleLoginProvider, FacebookLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { HelperballService } from '../service/helperball.service';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  loginForm: FormGroup;
+  socialUser: SocialUser;
+  isLoggedin: boolean;  
+
+  constructor(
+    private helperballService: HelperballService, 
+    private router: Router,
+    private formBuilder: FormBuilder, 
+    private socialAuthService: SocialAuthService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });    
+    
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = (user != null);
+      console.log(this.socialUser);
+    });
+  }
+  
+  logOut(): void {
+    this.socialAuthService.signOut();
+    this.socialAuthService.authState.subscribe((res) => {
+    });
+    this.router.navigate(['main']);
+  }
+}
