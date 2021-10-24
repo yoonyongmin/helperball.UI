fd149595fbbf2ebc126e1af12fa2da1509034bf2
yoonyongmin <dydalszhffk@naver.com>
2021-10-24 19:20:43 +0900

    라우팅 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: 'main', component: SignInComponent},
+  {path: 'signUp', component: SignUpComponent},
+  {path: 'info', component: UserInfoComponent},
   {path: 'layout', component: LayoutComponent},
   {path: 'dashboard', component: DashboardComponent}
 ];
