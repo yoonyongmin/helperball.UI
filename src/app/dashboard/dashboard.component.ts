c0cdc9cdcc6b5cc98820949df4a5eab77a80a6a2
yoonyongmin <dydalszhffk@naver.com>
2021-11-01 21:48:36 +0900

    echarts 옵션 설정

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,9 @@ import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { HelperballService } from '../service/helperball.service';
 import { SocialAuthService } from 'angularx-social-login';
+import { EChartsOption } from 'echarts';
+import { ThemeOption } from 'ngx-echarts';
+import { CoolTheme } from './coolTheme';
 
 
 @Component({
@@ -27,6 +30,41 @@ export class DashboardComponent implements OnInit {
   position: any;
   age: any;
 
+  theme: string | ThemeOption;
+  coolTheme = CoolTheme;
+  options = {
+    title: {
+      text: '내 스탯',
+      subtext: '전체 기록',
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+      x: 'center',
+      y: 'bottom',
+      data: ['Goal', 'Shoot', 'Assist', 'Pass', 'Tackle', 'Intercept']
+    },
+    calculable: true,
+    series: [
+      {
+        name: 'area',
+        type: 'pie',
+        radius: [30, 110],
+        roseType: 'area',
+        data: [
+          { value: 10, name: 'Goal' },
+          { value: 5, name: 'Shoot' },
+          { value: 15, name: 'Assist' },
+          { value: 25, name: 'Pass' },
+          { value: 20, name: 'Tackle' },
+          { value: 35, name: 'Intercept' },
+        ]
+      }
+    ]
+  };
 
   constructor(
     private helperballService: HelperballService,
